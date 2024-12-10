ef62df731825cebb88ff1cd575b3ad12c0fc807a
Serhat Uludag <serhat3264@gmail.com>
2024-12-10 12:52:25 +0300

    API Service Implemented

---
+++ app/src/main/java/com/serhatuludag/watchlistkeeper/service/RetrofitAPI.kt
@@ -0,0 +1,16 @@
+package com.serhatuludag.watchlistkeeper.service
+
+import com.serhatuludag.watchlistkeeper.model.ImageResponse
+import com.serhatuludag.watchlistkeeper.util.Constants.API_KEY
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RetrofitAPI {
+
+    @GET("/api/")
+    suspend fun imageSearch(
+        @Query("q") searchQuery: String,
+        @Query("key") apiKey : String = API_KEY) : Response<ImageResponse>
+
+}
---
+++ app/src/main/java/com/serhatuludag/watchlistkeeper/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.serhatuludag.watchlistkeeper.util
+
+object Constants {
+
+    const val API_KEY = "47557941-0e876d0581fa0041963e48bc7"
+    const val BASE_URL = "https://pixabay.com"
+}
